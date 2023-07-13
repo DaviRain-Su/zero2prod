e3e85ac63bdeb47ee085b5e83ea96b89c47dafd3
Davirain <davirain.yin@gmail.com>
2023-07-14 01:57:24 +0800

    add /:name handle

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,29 @@
 use anyhow::Result;
-use axum::{routing::get, Router};
+use axum::{extract::Path, response::IntoResponse, routing::get, Router};
+use std::net::SocketAddr;
+
+async fn index() -> &'static str {
+    "Hello, World!"
+}
+
+async fn greet(Path(name): Path<String>) -> impl IntoResponse {
+    format!("Hello, {}!", name)
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new()
+        .route("/", get(index))
+        .route("/:name", get(greet));
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    tracing_subscriber::fmt::init();
+
+    println!("listening on {}", addr);
 
     // run it with hyper on localhost:3000
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await?;
 
