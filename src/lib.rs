45177946b85b5b828e7fbe0a13e24bf8744fa68f
Davirain <davirain.yin@gmail.com>
2023-07-14 18:19:11 +0800

    Refactor routes and remove unused code in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,3 @@
-use anyhow::Result;
-use axum::routing::IntoMakeService;
-use axum::{
-    routing::{get, post},
-    Router, Server,
-};
-use hyper::server::conn::AddrIncoming;
-use std::net::TcpListener;
-
 pub mod configuration;
 pub mod routes;
 pub mod startup;
-
-use routes::greet;
-use routes::health_check;
-use routes::index;
-use routes::subscribe;
-
-pub fn run(listener: TcpListener) -> Result<Server<AddrIncoming, IntoMakeService<Router>>> {
-    tracing::info!("listener: {:?}", listener);
-
-    // build our application with a single route
-    let app = Router::new()
-        .route("/", get(index))
-        .route("/:name", get(greet))
-        .route("/health_check", get(health_check))
-        .route("/subscriptions", post(subscribe));
-
-    // run it with hyper on localhost:3000
-    let server = axum::Server::from_tcp(listener)?.serve(app.into_make_service());
-
-    Ok(server)
-}
