f296abce33a20524e8dc61e1e24a3314157611de
Davirain <davirain.yin@gmail.com>
2023-07-14 21:45:41 +0800

    Refactor subscription routes and add connection pool extraction

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,8 +1,13 @@
 use axum::body::Body;
 use axum::http::Response;
-use axum::http::StatusCode;
-use axum::{extract::Form, response::IntoResponse};
+use axum::http::{request::Parts, StatusCode};
+use axum::{
+    async_trait,
+    extract::{Form, FromRef, FromRequestParts, State},
+    response::IntoResponse,
+};
 use serde::Deserialize;
+use sqlx::postgres::PgPool;
 
 #[derive(Deserialize)]
 pub struct FormData {
@@ -10,8 +15,52 @@ pub struct FormData {
     email: String,
 }
 
+// we can extract the connection pool with `State`
+pub async fn using_connection_pool_extractor(
+    State(pool): State<PgPool>,
+) -> Result<String, (StatusCode, String)> {
+    sqlx::query_scalar("select 'hello world from pg'")
+        .fetch_one(&pool)
+        .await
+        .map_err(internal_error)
+}
+
+// we can also write a custom extractor that grabs a connection from the pool
+// which setup is appropriate depends on your application
+pub struct DatabaseConnection(sqlx::pool::PoolConnection<sqlx::Postgres>);
+
+#[async_trait]
+impl<S> FromRequestParts<S> for DatabaseConnection
+where
+    PgPool: FromRef<S>,
+    S: Send + Sync,
+{
+    type Rejection = (StatusCode, String);
+
+    async fn from_request_parts(_parts: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
+        let pool = PgPool::from_ref(state);
+
+        let conn = pool.acquire().await.map_err(internal_error)?;
+
+        Ok(Self(conn))
+    }
+}
+
+/// Utility function for mapping any error into a `500 Internal Server Error`
+/// response.
+fn internal_error<E>(err: E) -> (StatusCode, String)
+where
+    E: std::error::Error,
+{
+    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
+}
+
+// 这里我的疑问是，这里的两个参数的位置不能颠倒
 // Let's start simple: we always return a 200 OK
-pub async fn subscribe(form: Option<Form<FormData>>) -> impl IntoResponse {
+pub async fn subscribe(
+    DatabaseConnection(_conn): DatabaseConnection,
+    form: Option<Form<FormData>>,
+) -> impl IntoResponse {
     // Here you can use the form data.
     match form {
         Some(form) => {
--- src/startup.rs
+++ src/startup.rs
@@ -1,8 +1,5 @@
 use anyhow::Result;
-use axum::{
-    routing::{get, post},
-    Router,
-};
+use axum::{routing::get, Router};
 use sqlx::Pool;
 use sqlx::Postgres;
 use std::net::TcpListener;
@@ -10,7 +7,7 @@ use std::net::TcpListener;
 use crate::routes::greet;
 use crate::routes::health_check;
 use crate::routes::index;
-use crate::routes::subscribe;
+use crate::routes::{subscribe, using_connection_pool_extractor};
 
 pub async fn run(listener: TcpListener, conn_pool: Pool<Postgres>) -> Result<()> {
     tracing::info!("listener: {:?}", listener);
@@ -20,7 +17,10 @@ pub async fn run(listener: TcpListener, conn_pool: Pool<Postgres>) -> Result<()>
         .route("/", get(index))
         .route("/:name", get(greet))
         .route("/health_check", get(health_check))
-        .route("/subscriptions", post(subscribe))
+        .route(
+            "/subscriptions",
+            get(using_connection_pool_extractor).post(subscribe),
+        )
         .with_state(conn_pool); // ref: https://github.com/tokio-rs/axum/blob/main/examples/sqlx-postgres/src/main.rs#L27
 
     // run it with hyper on localhost:3000
