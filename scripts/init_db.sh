099ad6f33e6f7aeeaf609f0c2dd3473844af47cf
Davirain <davirain.yin@gmail.com>
2023-07-17 22:21:01 +0800

    Refactor init_db.sh script for using container IP to connect to Postgres

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -30,28 +30,37 @@ DB_PORT="${POSTGRES_PORT:=5432}"
 # Allow to skip Docker if a dockerized Postgres database is already running
 if [[ -z "${SKIP_DOCKER}" ]]
 then
-    docker run \
+   # 启动 Postgres 并保存容器 ID
+    CONTAINER_ID=$(docker run \
         -e POSTGRES_USER=${DB_USER} \
         -e POSTGRES_PASSWORD=${DB_PASSWORD} \
         -e POSTGRES_DB=${DB_NAME} \
-        -p "${DB_PORT}":5432 \
         -d postgres \
-        postgres -N 1000
-        # ^ Increased maximum number of connections for testing purposes
+        postgres -N 1000 \
+        -p "${DB_PORT}":5432)
+
 fi
+
+# 获取容器的 IP 地址
+CONTAINER_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$CONTAINER_ID")
+
 
 # Keep pinging Postgres until it's ready to accept commands
 export PGPASSWORD="${DB_PASSWORD}"
 
 
-until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+# 使用容器的 IP 地址来检查 Postgres 是否可用
+until psql -h "$CONTAINER_IP" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
     >&2 echo "Postgres is still unavailable - sleeping"
     sleep 1
 done
 
+
 >&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations now!"
 
-export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
+# 使用容器的 IP 地址来连接到 Postgres
+export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${CONTAINER_IP}:${DB_PORT}/${DB_NAME}
+
 sqlx database create
 sqlx migrate run
 
