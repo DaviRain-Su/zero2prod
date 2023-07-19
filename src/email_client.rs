d61179b9c85722296baaf896b794bfae652afc99
Davirain <davirain.yin@gmail.com>
2023-07-19 18:47:47 +0800

    Refactor email client to use `reqwest` for HTTP requests

--- src/email_client.rs
+++ src/email_client.rs
@@ -1,11 +1,23 @@
 use crate::domain::SubscriberEmail;
 use anyhow::Result;
+use reqwest::Client;
 
+#[derive(Debug)]
 pub struct EmailClient {
+    http_client: Client,
+    base_url: String,
     sender: SubscriberEmail,
 }
 
 impl EmailClient {
+    pub fn new(base_url: String, sender: SubscriberEmail) -> Self {
+        Self {
+            http_client: Client::new(),
+            base_url,
+            sender,
+        }
+    }
+
     pub async fn send_email(
         &self,
         recipient: SubscriberEmail,
