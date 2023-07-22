d31482c0330973d01b9f19707c40df19e0da8d58
Davirain <davirain.yin@gmail.com>
2023-07-22 22:21:47 +0800

    Refactor startup to build connection pool and perform initialization asynchronously

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 use anyhow::Result;
-use sqlx::postgres::PgPoolOptions;
-use std::net::TcpListener;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 use zero2prod::configuration::get_configuration;
-use zero2prod::email_client::EmailClient;
 use zero2prod::startup;
 
 #[tokio::main]
@@ -18,30 +15,5 @@ async fn main() -> Result<()> {
 
     let configuration = get_configuration()?;
 
-    let connection_pool = PgPoolOptions::new()
-        .acquire_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy_with(configuration.database.with_db());
-
-    // Build an `EmailClient` using `configuration`
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .map_err(|_| anyhow::anyhow!("Invalid sender email address."))?;
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        // pass new argument from configuration
-        timeout,
-    )?;
-
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-
-    let listener = TcpListener::bind(address)?;
-
-    startup::run(listener, connection_pool, email_client).await
+    startup::build(&configuration).await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,8 @@
+use crate::configuration::{DatabaseSettings, Settings};
 use anyhow::Result;
 use axum::extract::Form;
 use axum::{routing::get, Router};
+use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use tower_http::trace::{DefaultMakeSpan, TraceLayer};
@@ -10,6 +12,40 @@ use crate::routes::greet;
 use crate::routes::health_check;
 use crate::routes::index;
 use crate::routes::{subscribe, using_connection_pool_extractor};
+
+pub fn get_connection_poll(database_configuration: &DatabaseSettings) -> PgPool {
+    PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy_with(database_configuration.with_db())
+}
+
+pub async fn build(configuration: &Settings) -> Result<()> {
+    let connection_pool = get_connection_poll(&configuration.database);
+    // Build an `EmailClient` using `configuration`
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .map_err(|_| anyhow::anyhow!("Invalid sender email address."))?;
+    let timeout = configuration.email_client.timeout();
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url.clone(),
+        sender_email,
+        configuration.email_client.authorization_token.clone(),
+        // pass new argument from configuration
+        timeout,
+    )?;
+
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
+
+    let listener = TcpListener::bind(address)?;
+
+    run(listener, connection_pool, email_client).await?;
+
+    Ok(())
+}
 
 pub async fn run(
     listener: TcpListener,
